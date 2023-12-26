4fe47277a847fe033f3a2df6befb6e934990ff74
cisuno7 <cisuno7@hotmail.com>
2023-12-26 10:03:18 -0300

    credencias do banco online inseridas

--- DB/db.js
+++ DB/db.js
@@ -1,15 +1,14 @@
 const sql = require('mssql');
 
 const dbConfig = {
-    user: 'Jordan3', 
-    password: 'Ga771421', 
-    server: 'LAPTOP-UH10NKQD', 
+    user: 'sa', // Remova o espaço após 'sa'
+    password: 'DJ8:W^0[rT.3', // Verifique se esta é a senha correta
+    server: 'node163450-env-0304409.jelastic.saveincloud.net', // Use o host do SaveInCloud
+    port: 13914, // Use a porta pública configurada no SaveInCloud
     database: 'user_db',
     options: {
-        trustedConnection: true,
-        encrypt: true,
-        trustServerCertificate: true, 
-        rejectUnauthorized: false
+        encrypt: true, // Pode ser necessário para conexões seguras, dependendo da configuração do servidor
+        trustServerCertificate: true // True se estiver usando certificados autoassinados, false se não
     }
 };
 
--- Models/User.js
+++ Models/User.js
@@ -12,28 +12,26 @@ class User {
     }
     static async createUser({ name, email, password, identityDocument, documentType, nationality, phoneNumber }) {
         try {
-            if (!name || !email || !password || !identityDocument || !documentType || !nationality || !phoneNumber) {
-                throw new Error("Todos os campos são obrigatórios1.");
-            }
-    
+            const pool = await getConnection();
+            
+          
+            // Se não existir, prosseguir com a criação do usuário
             console.log("Senha recebida:", password);
             const hashedPassword = await bcrypt.hash(password, 10);
     
-            const pool = await getConnection();
             await pool.request()
-            .input('name', sql.VarChar, name)
-            .input('email', sql.VarChar, email)
-            .input('password', sql.VarChar, hashedPassword)
-            .input('identityDocument', sql.VarChar, identityDocument)
-            .input('documentType', sql.VarChar, documentType) 
-            .input('nationality', sql.VarChar, nationality) 
-            .input('phoneNumber', sql.VarChar, phoneNumber) 
-            .query('INSERT INTO tb_users (Name, Email, Password, IdentityDocument, DocumentType, Nationality, PhoneNumber) VALUES (@name, @email, @password, @identityDocument, @documentType, @nationality, @phoneNumber)');
+                .input('name', sql.VarChar, name)
+                .input('email', sql.VarChar, email)
+                .input('password', sql.VarChar, hashedPassword)
+                .input('identityDocument', sql.VarChar, identityDocument)
+                .query('INSERT INTO tb_users (Name, Email, Password) VALUES (@name, @email, @password)');
+    
         } catch (error) {
             console.error('Erro ao criar usuário:', error);
             throw error;
         }
     }
+    
 }    
 
 module.exports = User;
