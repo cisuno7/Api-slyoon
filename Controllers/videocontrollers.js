e88e9baf1254a828435c5480565bffe1246269a0
cisuno7 <cisuno7@hotmail.com>
2024-01-18 12:28:07 -0300

    subida do ssl

--- Controllers/videocontrollers.js
+++ Controllers/videocontrollers.js
@@ -7,7 +7,9 @@ const minioClient = new Minio.Client({
   endPoint: 'play.min.io',
   port: 9000,
   accessKey: 'Q3AM3UQ867SPQQA43P2F',
-  secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+  secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+  useSSL: true
+
 });
 
 // Configuração do Multer
