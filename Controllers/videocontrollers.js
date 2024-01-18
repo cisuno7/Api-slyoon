f12afd7eb3f4482513af4f7917427510c4b62b19
cisuno7 <cisuno7@hotmail.com>
2024-01-18 12:30:10 -0300

    certificado colocado

--- Controllers/videocontrollers.js
+++ Controllers/videocontrollers.js
@@ -9,7 +9,7 @@ const minioClient = new Minio.Client({
   accessKey: 'Q3AM3UQ867SPQQA43P2F',
   secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
   useSSL: true,
-  rejectUnauthorized: false,
+  rejectUnauthorized: false
 
 });
 
