c1749e22d2384d98a351f0b30ddfe208c129508b
cisuno7 <cisuno7@hotmail.com>
2024-01-18 12:29:30 -0300

    certificado de ssl colocado

--- Controllers/videocontrollers.js
+++ Controllers/videocontrollers.js
@@ -8,7 +8,8 @@ const minioClient = new Minio.Client({
   port: 9000,
   accessKey: 'Q3AM3UQ867SPQQA43P2F',
   secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
-  useSSL: true
+  useSSL: true,
+  rejectUnauthorized: false,
 
 });
 
