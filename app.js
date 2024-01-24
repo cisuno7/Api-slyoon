1d1d6ff51031448b9468414d339943b28e6fb657
cisuno7 <cisuno7@hotmail.com>
2024-01-23 23:45:32 -0300

    cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const authRoutes = require('./routes/authRoutes'); 
 const videoRoutes = require('./routes/videoRoutes');
