c89a1225ff4203c517997044a9dc647ef6dbf545
cisuno7 <cisuno7@hotmail.com>
2024-01-24 17:01:17 -0300

    container

--- Controllers/videocontrollers.js
+++ Controllers/videocontrollers.js
@@ -1,16 +1,12 @@
 const multer = require('multer');
-const Minio = require('minio');
+const AWS = require('aws-sdk');
 const VideoModel = require('../Models/videos');
 
-// Configuração do cliente Minio
-const minioClient = new Minio.Client({
-  endPoint: 'play.min.io',
-  port: 9000,
-  accessKey: 'Q3AM3UQ867SPQQA43P2F',
-  secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
-  useSSL: true,
-  rejectUnauthorized: false
-
+// Configuração do cliente AWS S3
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
 });
 
 const fileFilter = (req, file, cb) => {
@@ -23,9 +19,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 1048576000
-  },
+  limits: { fileSize: 1048576000 },
   fileFilter: fileFilter
 });
 
@@ -34,35 +28,33 @@ exports.uploadVideo = upload.single('videoFile');
 
 // Função para lidar com a lógica de upload
 exports.processVideoUpload = async (req, res) => {
-  // Validação do arquivo de vídeo
   try {
-    const {  title, description } = req.body;
+    const { title, description } = req.body;
     if (!req.file || !['video/mp4', 'video/mov', 'video/avi'].includes(req.file.mimetype)) {
       return res.status(400).json({ message: 'Formato de arquivo inválido' });
     }
-    
-    console.log(req.body)
-    console.log(title);
-    // Validate data
-  
-console.log(description);
-    // Upload do vídeo para o Minio
-    const bucketName = 'bookings';
-    const caminhoDoVideoNoMinio = 'bookings/' + req.file.originalname;
-    await minioClient.putObject(bucketName, caminhoDoVideoNoMinio, req.file.buffer);
+
+    // Upload do vídeo para o S3
+    const bucketName = process.env.BUCKET_NAME;
+    const videoPath = 'videos/' + req.file.originalname;
+    await s3.putObject({
+      Bucket: bucketName,
+      Key: videoPath,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype
+    }).promise();
 
     // Salvar informações do vídeo no banco de dados
     await VideoModel.saveVideoInfo({
       title,
       description,
-      uploaderId: req.user.id, // ID do usuário
-      videoPath: caminhoDoVideoNoMinio
+      uploaderId: req.user.id,
+      videoPath: videoPath
     });
 
     res.status(201).json({ message: 'Vídeo enviado com sucesso!' });
-  }catch (error) {
+  } catch (error) {
     console.error('Falha durante o upload/salvamento:', error);
     res.status(500).json({ message: 'Falha ao processar o vídeo', error: error.message });
-    }
-    
+  }
 };
